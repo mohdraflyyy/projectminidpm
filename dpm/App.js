55b3c80c9ba0ea61729c8833e407e8e94deec267
mohdraflyyy <95663750+mohdraflyyy@users.noreply.github.com>
2023-01-14 18:46:49 +0700

    Update App.js

--- dpm/App.js
+++ dpm/App.js
@@ -33,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fa8072',
     alignItems: 'center',
     justifyContent: 'center',
   },
